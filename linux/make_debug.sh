a426415d250c56bd432487cbc887093a5ac80a26
Walter Julius Hennecke <gsio01@Debian-60-squeeze-64-minimal.(none)>
2012-12-30 20:16:51 +0100

    Modified make file ...

--- linux/make_debug.sh
+++ linux/make_debug.sh
@@ -1,3 +1,1 @@
 make build_debug
-cp debugi386-glibc/linuxef2ded ../../../GAME/
-cp debugi386-glibc/ef2gamei386.so ../../../GAME/
--- linux/make_release.sh
+++ linux/make_release.sh
@@ -1,3 +1,1 @@
 make build_release
-cp releasei386-glibc/linuxef2ded ../../../GAME/
-cp releasei386-glibc/ef2gamei386.so ../../../GAME/
