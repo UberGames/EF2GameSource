aeb607dfb8b44a9310179953a570a05c06b2cee1
Walter Julius Hennecke <walter.julius@hennecke-online.net>
2014-03-09 23:06:51 +0100

    reformat

--- dlls/game/useAlarm.hpp
+++ dlls/game/useAlarm.hpp
@@ -38,108 +38,103 @@ class UseAlarm;
 //
 // Method of Use: Statemachine or another behavior
 //--------------------------------------------------------------
-class UseAlarm : public Behavior
-	{
+class UseAlarm : public Behavior {
+	public:
 	//------------------------------------
 	// States
 	//------------------------------------
-	public:
-		typedef enum
-		{
+	typedef enum {
 		USE_ALARM_FIND_NODE,
 		USE_ALARM_MOVE_TO_NODE,
-		USE_ALARM_AT_NODE,		
+		USE_ALARM_AT_NODE,
 		USE_ALARM_WAIT_ON_ANIM,
 		USE_ALARM_ROTATE_TO_ENEMY,
 		USE_ALARM_SUCCESSFUL,
 		USE_ALARM_FAILED
-		} useAlarmStates_t;
-
-	//------------------------------------
-	// Parameters
-	//------------------------------------
-	private: // Parameters
-		str							_movementAnimName ; // anim to play to move to work node, default is "walk"
-		float						_maxDistance;	    // maximum distance to look for node
-
-	//-------------------------------------
-	// Internal Functionality
-	//-------------------------------------
-	protected:
-		void	transitionToState	( useAlarmStates_t state );
-		void	setInternalState	( useAlarmStates_t state , const str &stateName );
-		void	init				( Actor &self );
-		void	think				();				
-
-		void					setupStateFindNode						();
-		BehaviorReturnCode_t	evaluateStateFindNode					();
-		void					failureStateFindNode					( const str& failureReason );
-
-		void					setupStateMoveToNode					();
-		BehaviorReturnCode_t	evaluateStateMoveToNode					();
-		void					failureStateMoveToNode					( const str& failureReason );
-
-		void					setupStateAtNode						();
-		BehaviorReturnCode_t	evaluateStateAtNode						();
-		void					failureStateAtNode						( const str& failureReason );
-
-		void					setupStateWaitOnAnim					();
-		BehaviorReturnCode_t	evaluateStateWaitOnAnim					();
-		void					failureStateWaitOnAnim					( const str& failureReason );
-
-		void					setupStateRotateToEnemy					();
-		BehaviorReturnCode_t	evaluateStateRotateToEnemy				();
-		void					failureStateRotateToEnemy				( const str& failureReason );
-
+	} useAlarmStates_t;
 
 	//-------------------------------------
 	// Public Interface
 	//-------------------------------------
-	public:
-		CLASS_PROTOTYPE( UseAlarm );
-												UseAlarm();
-											   ~UseAlarm();
+	CLASS_PROTOTYPE(UseAlarm);
+	UseAlarm();
+	~UseAlarm();
 
-		void									SetArgs				( Event *ev );      
-		void									AnimDone			( Event *ev );
-		void									HandleNodeCommand	( Event *ev );
+	void SetArgs(Event* ev);
+	void AnimDone(Event* ev);
+	void HandleNodeCommand(Event* ev);
 
-		void									Begin				( Actor &self );		
-		BehaviorReturnCode_t					Evaluate			( Actor &self );
-		void									End					( Actor &self );
+	void Begin(Actor& self);
+	BehaviorReturnCode_t Evaluate(Actor& self);
+	void End(Actor& self);
 
-		virtual void							Archive  ( Archiver &arc );
+	virtual void Archive(Archiver& arc);
 
-	private: // Component Behaviors
-		GotoHelperNode							_gotoHelperNode;
-		RotateToEntity							_rotateToEntity;
+	protected:
+	//-------------------------------------
+	// Internal Functionality
+	//-------------------------------------
+	void transitionToState(useAlarmStates_t state);
+	void setInternalState(useAlarmStates_t state, const str& stateName);
+	void init(Actor &self);
+	void think();
 
-	private: // Member Variables
-		HelperNodePtr							_node;            
-		unsigned int							_state;
-		bool									_animDone;
-		Actor*									_self;
-		
-	};
+	void setupStateFindNode();
+	BehaviorReturnCode_t evaluateStateFindNode();
+	void failureStateFindNode(const str& failureReason);
 
-inline void UseAlarm::Archive( Archiver &arc	)
-{
-	Behavior::Archive ( arc );	    
-   
+	void setupStateMoveToNode();
+	BehaviorReturnCode_t evaluateStateMoveToNode();
+	void failureStateMoveToNode(const str& failureReason);
+
+	void setupStateAtNode();
+	BehaviorReturnCode_t evaluateStateAtNode();
+	void failureStateAtNode(const str& failureReason);
+
+	void setupStateWaitOnAnim();
+	BehaviorReturnCode_t evaluateStateWaitOnAnim();
+	void failureStateWaitOnAnim(const str& failureReason);
+
+	void setupStateRotateToEnemy();
+	BehaviorReturnCode_t evaluateStateRotateToEnemy();
+	void failureStateRotateToEnemy(const str& failureReason);
+
+
+	private:
+	//------------------------------------
+	// Parameters
+	//------------------------------------
+	str _movementAnimName; // anim to play to move to work node, default is "walk"
+	float _maxDistance;	    // maximum distance to look for node
+ 
+	// Component Behaviors
+	GotoHelperNode _gotoHelperNode;
+	RotateToEntity _rotateToEntity;
+
+	// Member Variables
+	HelperNodePtr _node;
+	unsigned int _state;
+	bool _animDone;
+	Actor* _self;
+};
+
+inline void UseAlarm::Archive(Archiver& arc) {
+	Behavior::Archive(arc);
+
 	// Archive Parameters
-	arc.ArchiveString			( &_movementAnimName);
-	arc.ArchiveFloat			( &_maxDistance     );
+	arc.ArchiveString(&_movementAnimName);
+	arc.ArchiveFloat(&_maxDistance);
 
 	// Archive Components
-	arc.ArchiveObject			( &_gotoHelperNode  );
-	arc.ArchiveObject			( &_rotateToEntity  );
-	
+	arc.ArchiveObject(&_gotoHelperNode);
+	arc.ArchiveObject(&_rotateToEntity);
+
 
 	// Archive Member Vars
-	arc.ArchiveSafePointer		( &_node				);
-	arc.ArchiveUnsigned			( &_state				);
-	arc.ArchiveBool				( &_animDone			);
-	arc.ArchiveObjectPointer	( ( Class ** )&_self	);
+	arc.ArchiveSafePointer(&_node);
+	arc.ArchiveUnsigned(&_state);
+	arc.ArchiveBool(&_animDone);
+	arc.ArchiveObjectPointer((Class **)&_self);
 }
 
 #endif /* __USE_ALARM__ */
