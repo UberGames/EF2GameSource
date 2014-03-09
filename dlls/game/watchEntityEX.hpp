32fe234248ebea11aea1374b00adb445cf94d6b9
Walter Julius Hennecke <walter.julius@hennecke-online.net>
2014-03-09 23:12:30 +0100

    reformat

--- dlls/game/watchEntityEX.hpp
+++ dlls/game/watchEntityEX.hpp
@@ -39,114 +39,104 @@ class WatchEntityEX;
 //
 // Method of Use:  Called From State Machine
 //--------------------------------------------------------------
-class WatchEntityEX : public Behavior
-	{
+class WatchEntityEX : public Behavior {
+	public:
 	//------------------------------------
 	// States
 	//------------------------------------
-	public:  
-		typedef enum
-		{
-			WATCH_ENTITY_EX_SETUP,
-			WATCH_ENTITY_EX_ROTATE,
-			WATCH_ENTITY_EX_HOLD,			
-			WATCH_ENTITY_EX_SUCCESS,
-			WATCH_ENTITY_EX_FAILED,
-		} watchEntityStates_t;
-
-	//------------------------------------
-	// Parameters
-	//------------------------------------
-	private:
-		str								_stance;
-		str								_shuffleAnim;
-		str								_torsoAnim;
-		EntityPtr						_ent;
-
-	//-------------------------------------
-	// Internal Functionality
-	//-------------------------------------
-	protected:
-		void	transitionToState	( watchEntityStates_t state );
-		void	setInternalState	( watchEntityStates_t state , const str &stateName );
-		void	init				( Actor &self );
-		void	think				();		
-		
-		void					setupStateSetup				();
-		BehaviorReturnCode_t	evaluateStateSetup			();
-		void					failureStateSetup			( const str& failureReason );
-
-		void					setupStateRotate			();
-		BehaviorReturnCode_t	evaluateStateRotate			();
-		void					failureStateRotate			( const str& failureReason );
-
-		void					setupStateHold				();
-		BehaviorReturnCode_t	evaluateStateHold			();
-		void					failureStateHold			( const str& failureReason );
-
-
-	
+	typedef enum {
+		WATCH_ENTITY_EX_SETUP,
+		WATCH_ENTITY_EX_ROTATE,
+		WATCH_ENTITY_EX_HOLD,
+		WATCH_ENTITY_EX_SUCCESS,
+		WATCH_ENTITY_EX_FAILED,
+	} watchEntityStates_t;
 
 	//-------------------------------------
 	// Public Interface
 	//-------------------------------------
-	public:
-		CLASS_PROTOTYPE( WatchEntityEX );
+	CLASS_PROTOTYPE(WatchEntityEX);
 
-										WatchEntityEX();
-									   ~WatchEntityEX();
+	WatchEntityEX();
+	~WatchEntityEX();
 
-		void							SetArgs					( Event *ev );      
-		void							AnimDone				( Event *ev );
+	void SetArgs(Event* ev);
+	void AnimDone(Event* ev);
 
-		void							Begin					( Actor &self );		
-		BehaviorReturnCode_t			Evaluate				( Actor &self );
-		void							End						( Actor &self );
-		
-		void							SetEntity				( Entity *ent );
+	void Begin(Actor& self);
+	BehaviorReturnCode_t Evaluate(Actor& self);
+	void End(Actor& self);
 
-		virtual void					Archive  ( Archiver &arc );
+	void SetEntity(Entity* ent);
+
+	virtual void Archive(Archiver& arc);
+
+	protected:
+	//-------------------------------------
+	// Internal Functionality
+	//-------------------------------------
+	void transitionToState(watchEntityStates_t state);
+	void setInternalState(watchEntityStates_t state, const str& stateName);
+	void init(Actor& self);
+	void think();
+
+	void setupStateSetup();
+	BehaviorReturnCode_t evaluateStateSetup();
+	void failureStateSetup(const str& failureReason);
+
+	void setupStateRotate();
+	BehaviorReturnCode_t evaluateStateRotate();
+	void failureStateRotate(const str& failureReason);
+
+	void setupStateHold();
+	BehaviorReturnCode_t evaluateStateHold();
+	void failureStateHold(const str& failureReason);
+
+	private:
+	//------------------------------------
+	// Parameters
+	//------------------------------------
+	str _stance;
+	str _shuffleAnim;
+	str _torsoAnim;
+	EntityPtr _ent;
 
 	//-------------------------------------
 	// Components
 	//-------------------------------------
-	private: 		
-		RotateToEntity					_rotateToEntity;
+	RotateToEntity _rotateToEntity;
 
 	//-------------------------------------
 	// Member Variables
 	//-------------------------------------
-	private: 
-		unsigned int					_state;		
-		float							_time;
-		float							_turnspeed;	
+	unsigned int _state;
+	float _time;
+	float _turnspeed;
+};
 
-	};
-
-inline void WatchEntityEX::SetEntity( Entity *ent )
-{
-	if ( ent )
+inline void WatchEntityEX::SetEntity(Entity* ent) {
+	if (ent) {
 		_ent = ent;
+	}
 }
 
-inline void WatchEntityEX::Archive( Archiver &arc	)
-{
-	Behavior::Archive ( arc );	     
+inline void WatchEntityEX::Archive(Archiver& arc) {
+	Behavior::Archive(arc);
 
 	// Archive Parameters
-	arc.ArchiveString			( &_stance	  );
-	arc.ArchiveString			( &_shuffleAnim	  );
-	arc.ArchiveString			( &_torsoAnim );
-	arc.ArchiveSafePointer		( &_ent		  );
+	arc.ArchiveString(&_stance);
+	arc.ArchiveString(&_shuffleAnim);
+	arc.ArchiveString(&_torsoAnim);
+	arc.ArchiveSafePointer(&_ent);
 
 	// Archive Components	
-	arc.ArchiveObject			( &_rotateToEntity			);
+	arc.ArchiveObject(&_rotateToEntity);
 
 	// Archive Member Vars      
-	arc.ArchiveUnsigned			( &_state					);
-	arc.ArchiveFloat			( &_time					);
-	arc.ArchiveFloat			( &_turnspeed				);
-	
+	arc.ArchiveUnsigned(&_state);
+	arc.ArchiveFloat(&_time);
+	arc.ArchiveFloat(&_turnspeed);
+
 }
 
 
