b855763ada0fe21ffa4a1b36150f11d89bf5d54e
Walter Julius Hennecke <walter.julius@hennecke-online.net>
2014-03-09 19:13:47 +0100

    reformatting

--- dlls/game/gotoHelperNode.hpp
+++ dlls/game/gotoHelperNode.hpp
@@ -37,122 +37,112 @@ class GotoHelperNode;
 //
 // Method of Use:  Called From State Machine
 //--------------------------------------------------------------
-class GotoHelperNode : public Behavior
-	{
+class GotoHelperNode : public Behavior {
+	public:
 	//------------------------------------
 	// States
 	//------------------------------------
-	public:  
-		typedef enum
-		{
-			GOTO_HNODE_FIND_NODE,
-			GOTO_HNODE_MOVE_TO_NODE,
-			GOTO_HNODE_SUCCESS,
-			GOTO_HNODE_FAILED
-		} GotoHelperNodeStates_t;
+	typedef enum {
+		GOTO_HNODE_FIND_NODE,
+		GOTO_HNODE_MOVE_TO_NODE,
+		GOTO_HNODE_SUCCESS,
+		GOTO_HNODE_FAILED
+	} GotoHelperNodeStates_t;
 
-	//------------------------------------
-	// Parameters
-	//------------------------------------
-	private:
-		str			    _nodeType;
-		str				_movementAnim;
-		HelperNodePtr	_node;
-		float			_maxDistance;
+	//-------------------------------------
+	// Public Interface
+	//-------------------------------------
+	CLASS_PROTOTYPE(GotoHelperNode);
+
+	GotoHelperNode();
+	~GotoHelperNode();
+
+	void SetArgs(Event* ev);
+	void AnimDone(Event* ev);
+
+	void Begin(Actor& self);
+	BehaviorReturnCode_t Evaluate(Actor& self);
+	void End(Actor& self);
+
+	// Accessors
+	void SetNode(HelperNode* node);
+	void SetMovementAnim(const str& anim);
+
+	virtual void Archive(Archiver& arc);
 
 	//-------------------------------------
 	// Internal Functionality
 	//-------------------------------------
 	protected:
-		void	transitionToState	( GotoHelperNodeStates_t state );
-		void	setInternalState	( GotoHelperNodeStates_t state , const str &stateName );
-		void	init				( Actor &self );
-		void	think				();		
-	
-
-		void					setupStateMoveToNode		();
-		BehaviorReturnCode_t	evaluateStateMoveToNode		();
-		void					failureStateFindNode		( const str& failureReason );
-
-		void					setupStateFindNode		    ();
-		BehaviorReturnCode_t    evaluateStateFindNode       ();
-		void					failureStateMoveToNode		( const str& failureReason );
-
-	//-------------------------------------
-	// Public Interface
-	//-------------------------------------
-	public:
-		CLASS_PROTOTYPE( GotoHelperNode );
-
-										GotoHelperNode();
-									   ~GotoHelperNode();
-
-		void							SetArgs			( Event *ev );      
-		void							AnimDone		( Event *ev );
-	
-		void							Begin			( Actor &self );		
-		BehaviorReturnCode_t			Evaluate		( Actor &self );
-		void							End				( Actor &self );
-
-		// Accessors
-		void							SetNode			( HelperNode *node );
-		void							SetMovementAnim ( const str &anim );
+	void transitionToState(GotoHelperNodeStates_t state);
+	void setInternalState(GotoHelperNodeStates_t state, const str& stateName);
+	void init(Actor& self);
+	void think();
 
 
-		virtual void					Archive  ( Archiver &arc );
+	void setupStateMoveToNode();
+	BehaviorReturnCode_t evaluateStateMoveToNode();
+	void failureStateFindNode(const str& failureReason);
+
+	void setupStateFindNode();
+	BehaviorReturnCode_t evaluateStateFindNode();
+	void failureStateMoveToNode(const str& failureReason);
+
+	private:
+	//------------------------------------
+	// Parameters
+	//------------------------------------
+	str _nodeType;
+	str _movementAnim;
+	HelperNodePtr _node;
+	float _maxDistance;
 
 	//-------------------------------------
 	// Components
 	//-------------------------------------
-	private: 	
-		GotoPoint						_gotoPoint;
+	GotoPoint _gotoPoint;
 
 
 	//-------------------------------------
 	// Member Variables
 	//-------------------------------------
-	private: 
-		GotoHelperNodeStates_t			_state;		 				
-		Actor						   *_self;		
+	GotoHelperNodeStates_t _state;
+	Actor* _self;
 
-		static const float				NODE_RADIUS;
+	static const float NODE_RADIUS;
+};
 
-	};
-
-inline void GotoHelperNode::SetNode ( HelperNode *node )
-{
-	assert ( node );
+inline void GotoHelperNode::SetNode(HelperNode* node) {
+	assert(node);
 	_node = node;
 }
 
-inline void	GotoHelperNode::SetMovementAnim ( const str &anim )
-{
+inline void	GotoHelperNode::SetMovementAnim(const str& anim) {
 	_movementAnim = anim;
 }
 
 
-inline void GotoHelperNode::Archive( Archiver &arc	)
-{
-	Behavior::Archive ( arc );	     
+inline void GotoHelperNode::Archive(Archiver& arc) {
+	Behavior::Archive(arc);
 
 	//
 	// Archive Parameters
 	//
-	arc.ArchiveString( &_nodeType );
-	arc.ArchiveString		( &_movementAnim );
-	arc.ArchiveSafePointer	( &_node );
-	arc.ArchiveFloat( &_maxDistance );
+	arc.ArchiveString(&_nodeType);
+	arc.ArchiveString(&_movementAnim);
+	arc.ArchiveSafePointer(&_node);
+	arc.ArchiveFloat(&_maxDistance);
 
 	//
 	// Archive Components
 	//
-	arc.ArchiveObject		( &_gotoPoint );
+	arc.ArchiveObject(&_gotoPoint);
 
 	//
 	// Archive Member Variables
 	//
-	ArchiveEnum				( _state, GotoHelperNodeStates_t   );	
-	arc.ArchiveObjectPointer( ( Class ** )&_self			);
+	ArchiveEnum(_state, GotoHelperNodeStates_t);
+	arc.ArchiveObjectPointer((Class **)&_self);
 }
 
 #endif /* __GOTO_HELPER_NODE___ */
