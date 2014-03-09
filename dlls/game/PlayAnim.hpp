51e951317782c1e1b294af1f7de9393930c72939
Walter Julius Hennecke <walter.julius@hennecke-online.net>
2014-03-09 22:34:29 +0100

    reformating

--- dlls/game/PlayAnim.hpp
+++ dlls/game/PlayAnim.hpp
@@ -37,66 +37,62 @@ class PlayAnim;
 //
 // Method of Use:  Called From State Machine
 //--------------------------------------------------------------
-class PlayAnim : public Behavior
-{
+class PlayAnim : public Behavior {
+	public:
 	//------------------------------------
 	// States
 	//------------------------------------
-	public:
-		typedef enum
-		{	
-			PLAYANIM_SETUP,
-			PLAYANIM_ANIMATE,
-			PLAYANIM_SUCCESS,
-			PLAYANIM_FAILED
-		} PlayAnimStates_t;
-
-	//------------------------------------
-	// Parameters
-	//------------------------------------
-	private: 
-		str							_legAnim;  
-		str							_torsoAnim;
-		float						_minTime;
-		float						_maxTime;
-
-	//-------------------------------------
-	// Internal Functionality
-	//-------------------------------------
-	protected:
-		void	transitionToState	( PlayAnimStates_t state );
-		void	setInternalState	( PlayAnimStates_t state , const str &stateName );
-		void	init				( Actor &self );
-		void	think				();
-	
-		void					setupStateSetup				();
-		BehaviorReturnCode_t	evaluateStateSetup			();
-		void					failureStateSetup			( const str& failureReason );
-
-		void					setupStateAnimate			();
-		BehaviorReturnCode_t	evaluateStateAnimate		();
-		void					failureStateAnimate			( const str& failureReason );
-
+	typedef enum {
+		PLAYANIM_SETUP,
+		PLAYANIM_ANIMATE,
+		PLAYANIM_SUCCESS,
+		PLAYANIM_FAILED
+	} PlayAnimStates_t;
 
 	//-------------------------------------
 	// Public Interface
 	//-------------------------------------
-	public:
-		CLASS_PROTOTYPE( PlayAnim );
+	CLASS_PROTOTYPE(PlayAnim);
 
-												PlayAnim();
-											   ~PlayAnim();
+	PlayAnim();
+	~PlayAnim();
 
-		void									SetArgs		( Event *ev     );      
-		void									Begin		( Actor &self   );		
-		BehaviorReturnCode_t					Evaluate	( Actor &self   );
-		void									End			( Actor &self   );
-		virtual void							Archive		( Archiver &arc );
+	void SetArgs(Event* ev);
+	void Begin(Actor& self);
+	BehaviorReturnCode_t Evaluate(Actor& self);
+	void End(Actor& self);
+	virtual void Archive(Archiver& arc);
 
-		void									setAnim		( const str &animName ) { _legAnim = animName;		}
-		void									setTorsoAnim( const str &animName ) { _torsoAnim = animName;	}
-		void									setMinTime	( float minTime		  ) { _minTime = minTime;		}
-		void									setMaxTime	( float maxTime       ) { _maxTime = maxTime;		}
+	void setAnim(const str& animName) { _legAnim = animName; }
+	void setTorsoAnim(const str& animName) { _torsoAnim = animName; }
+	void setMinTime(float minTime) { _minTime = minTime; }
+	void setMaxTime(float maxTime) { _maxTime = maxTime; }
+
+	protected:
+	//-------------------------------------
+	// Internal Functionality
+	//-------------------------------------
+	void transitionToState(PlayAnimStates_t state);
+	void setInternalState(PlayAnimStates_t state, const str& stateName);
+	void init(Actor& self);
+	void think();
+
+	void setupStateSetup();
+	BehaviorReturnCode_t evaluateStateSetup();
+	void failureStateSetup(const str& failureReason);
+
+	void setupStateAnimate();
+	BehaviorReturnCode_t evaluateStateAnimate();
+	void failureStateAnimate(const str& failureReason);
+
+	private:
+	//------------------------------------
+	// Parameters
+	//------------------------------------
+	str _legAnim;
+	str _torsoAnim;
+	float _minTime;
+	float _maxTime;
 
 	//-------------------------------------
 	// Components
@@ -105,29 +101,27 @@ class PlayAnim : public Behavior
 	//-------------------------------------
 	// Member Variables
 	//-------------------------------------
-	private: 
-		PlayAnimStates_t						_state;	
-		float									_endTime;
-		
+	PlayAnimStates_t _state;
+	float _endTime;
+
 };
 
-inline void PlayAnim::Archive( Archiver &arc	)
-{
-	Behavior::Archive( arc );	 
-   
-   // Archive Parameters
-	arc.ArchiveString ( &_legAnim );
-	arc.ArchiveString ( &_torsoAnim );
-	arc.ArchiveFloat  ( &_minTime );
-	arc.ArchiveFloat  ( &_maxTime);
+inline void PlayAnim::Archive(Archiver& arc) {
+	Behavior::Archive(arc);
+
+	// Archive Parameters
+	arc.ArchiveString(&_legAnim);
+	arc.ArchiveString(&_torsoAnim);
+	arc.ArchiveFloat(&_minTime);
+	arc.ArchiveFloat(&_maxTime);
 
 
-   // Archive Components
+	// Archive Components
 
-   // Archive Member Variables
-	ArchiveEnum					( _state, PlayAnimStates_t		);
-	arc.ArchiveFloat			( &_endTime						);
-}  
+	// Archive Member Variables
+	ArchiveEnum(_state, PlayAnimStates_t);
+	arc.ArchiveFloat(&_endTime);
+}
 
 #endif /* __PLAY_ANIM_HPP__ */
 
